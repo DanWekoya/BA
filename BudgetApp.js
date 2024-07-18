079cd18ddba6c89490e1b4588e1046cff4c31241
Dan Wekoya <142000471+DanWekoya@users.noreply.github.com>
2024-07-18 14:34:50 +0300

    Update BudgetApp.js
    Added a function that computes amount spent so far and remaining amount

--- BudgetApp.js
+++ BudgetApp.js
@@ -9,3 +9,17 @@ function BudgetValidation() {
   }
 }
 window.onload = "BudgetValidation()";
+
+
+function SaveBudget (){
+let Transport = Number (document.getElementById("TransportAllocation").value);
+let Internet = Number (document.getElementById("InternetAllocation").value); 
+let Water = Number (document.getElementById("WaterAllocation").value);
+let Power = Number (document.getElementById("PowerAllocation").value);
+let Other = Number (document.getElementById("OtherAllocation").value);
+let Total = Transport + Internet + Water + Power + Other ;
+let Remaining = 20000 - Total ;
+document.getElementById("Spent").innerHTML = "Spent: $" + Total;
+document.getElementById("Remaining").innerHTML = "Remaining: $" + Remaining;
+ }
+ window.onload = "SaveBudget()";
